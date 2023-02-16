178fed0fc7d762a731c50165b556a0676bc0301e
Joe Jeyaseelan <61643920+jolan94@users.noreply.github.com>
2023-02-16 05:36:46 +0000

    avatar url option

--- server.js
+++ server.js
@@ -22,6 +22,29 @@ app.get('/avatar', async (req, res) => {
   }
 });
 
+app.get('/avatarurl', (req, res) => {
+  axios({
+    method: 'get',
+    url: 'https://thispersondoesnotexist.com/image',
+    responseType: 'stream'
+  })
+    .then(function (response) {
+      const file_name = uuidv4() + '.jpg'; // Use a random file name if needed
+      const stream = fs.createWriteStream(file_name);
+      response.data.pipe(stream);
+      // const url = `http://localhost:${port}/${file_name}`;
+      const url = `https://randomavatar.onrender.com/${file_name}`;
+      console.log(url); // Print the URL to console
+      // res.send(`Avatar URL printed to console ${url}`);
+      res.send(url);
+    })
+    .catch(function (error) {
+      console.log(error);
+      res.status(500).send('Error generating avatar');
+    });
+});
+
+
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
@@ -36,27 +59,7 @@ app.listen(PORT, () => {
 // const app = express();
 // const port = 3000;
 
-// app.get('/', (req, res) => {
-//   axios({
-//     method: 'get',
-//     url: 'https://thispersondoesnotexist.com/image',
-//     responseType: 'stream'
-//   })
-//     .then(function (response) {
-//       const file_name = uuidv4() + '.jpg'; // Use a random file name if needed
-//       const stream = fs.createWriteStream(file_name);
-//       response.data.pipe(stream);
-//       // const url = `http://localhost:${port}/${file_name}`;
-//       const url = `https://randomavatar.onrender.com/${file_name}`;
-//       console.log(url); // Print the URL to console
-//       // res.send(`Avatar URL printed to console ${url}`);
-//       res.send(url);
-//     })
-//     .catch(function (error) {
-//       console.log(error);
-//       res.status(500).send('Error generating avatar');
-//     });
-// });
+
 
 // app.use(express.static(__dirname));
 
